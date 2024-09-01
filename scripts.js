c265d6dd8627db96e35e4c39f15df2e39501f29a
matheusrcs25 <matheusrcs25@gmail.com>
2024-09-01 09:15:22 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,14 @@
+function generateNumber() {
+
+    const min = Math.ceil(document.querySelector('.input-min').value)
+    const max = Math.floor(document.querySelector('.input-max').value)
+
+    if (min >= max) {
+        alert ('O valor mínimo deve ser MENOR que o valor MÁXIMO!')
+    } else {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
+    
+        alert(result)
+    }
+    
+}
